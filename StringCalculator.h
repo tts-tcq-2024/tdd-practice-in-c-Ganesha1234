3a7757fba39f16a0d4349e1f677bd20cf8c2ddd9
Ganesha A S <93441611+Ganesha1234@users.noreply.github.com>
2024-07-10 23:12:24 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -4,9 +4,9 @@
 // Function to calculate sum of digits in a string
 int sum_of_digits(const char *str) {
     int sum = 0;
-    while (*str) {
-        sum += isdigit((unsigned char)*str) ? *str - '0' : 0;
-        str++;
-    }
+    //while (*str) {
+    //    sum += isdigit((unsigned char)*str) ? *str - '0' : 0;
+   //     str++;
+    //}
     return sum;
 }
